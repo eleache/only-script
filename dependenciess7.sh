9fa3c7aa697b8e76d0d91403dcd0b8c4855f35e3
Crybaby <89001242+eleache@users.noreply.github.com>
2021-10-08 10:22:11 -0500

    'patear el barandal del vecino'

---
+++ dependenciess7.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# dedicated to installing dependencies for security audits for educational purposes. Complement of Sigsploit (Attack tool for #SS7 / 4G LTE Telecom Signaling Exploitation Framework)
+# author.ofThisScript: eleache
+sudo apt-get install grass-core libosmo-sigtran-dev libosmo-sigtran-doc libosmo-sigtran5 libosmomtp0 libqgisgrass7-3.16.10 libss7-2.0 libss7-dev osmo-stp
+
+
+#grass-core 
+#libosmo-sigtran-dev - Development headers for the Osmocom SIGTRAN library
+#libosmo-sigtran-doc - Documentation for the Osmocom SIGTRAN library
+#libosmo-sigtran5 - Osmocom SIGTRAN library (SCCP, SUA, M3UA and more)
+#libosmomtp0 - Message Transfer Part for Signaling System 7
+#libqgisgrass7-3.16.10 - QGIS - shared grass library
+#libss7-2.0 - Signalling System 7 (ss7) library
+#libss7-dev - Signalling System 7 (ss7) development files
+#osmo-stp - Osmocom SIGTRAN STP (Signaling Transfer Point
